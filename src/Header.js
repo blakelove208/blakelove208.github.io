92ef9a2b378a6cd467eba529e36a98f94bd8445f
Blake <blakelove208@gmail.com>
2018-08-16 22:27:34 -0600

    Revert "edit titling"
    This reverts commit 3c96f1ee70a6ddafc7359496f403b8a91fad3545.

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ const Header = () => (
     <ul className="main-nav desktop">
       <li><NavLink exact to="/"><span>Projects</span></NavLink></li>
       <li><NavLink to="/skills"><span>Skills</span></NavLink></li>
-      <h1 className="name">Blake Love</h1>
+      <h1 className="name">Blake Harris</h1>
       <li><NavLink to="/about"><span>About Me</span></NavLink></li>
       <li><NavLink to="/contact"><span>Contact</span></NavLink></li>
     </ul>
